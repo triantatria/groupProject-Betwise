b3684ba42a5a2ad88d779c37f45fc470921d9db8
Nathan Megersa <nathan.megersa@colorado.edu>
2025-11-13 11:37:38 -0700

    Added balance row to users table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS users (
     user_id      BIGSERIAL PRIMARY KEY,
     username     VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT now()
+    created_at   TIMESTAMPTZ NOT NULL DEFAULT now(),
+    balance     BIGINT NOT NULL DEFAULT 0 CHECK (balance >= 0)
 );
 
 
